2c55be872fc42c4f060b5bf5343bd2573cf94be8
Shungheon Chad Chai <chad2212@gmail.com>
2016-04-23 10:16:19 -0500

    Static Pages added, ideas added, routes configured, nav bar created, some CSS done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#homepage'
+  get '/about' => 'static_pages#about_us', as: 'about'
+  resources :static_pages
+  resources :ideas
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
